33710dc2309183268937619f78e35325e17bc23e
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-03-30 12:47:06 +0300

    Introduced logic to generate abstract processor + refactoring.

--- ts/index.ts
+++ ts/index.ts
@@ -4,8 +4,17 @@ import { Question, Inquirer } from "inquirer";
 class PipelinesGenerator extends Generator {
     answersListener: Promise<Generator.Answers>;
 
-    initializing() {
+    constructor(args: string | string[], options: {}) {
+        super(args, options);
 
+        let subfolderArgumentConfig: Generator.OptionConfig = {
+            description: "Specififes, whether to create a subfolder for the current item.",
+            default: false
+        };
+        this.option("subfolder", subfolderArgumentConfig);
+    }
+
+    initializing() {
     }
 
     async prompting() {
@@ -41,23 +50,63 @@ class PipelinesGenerator extends Generator {
         var processorNames: string = answers["processorNames"];
         var processorNameStrings: string[] = processorNames.split(' ');
 
-        for (const processorName of processorNameStrings) {
-            this.fs.copyTpl(
-                this.templatePath('_predefinedProcessor.ts.txt'),
-                this.destinationPath('./processors/' + processorName + '.ts'),
-                {
-                    'pipelineName': pipelineName,
-                    'processorName': processorName
-                },
-                {});
+        this._createPipelineInfrastructure(pipelineName, processorNameStrings, this.options["subfolder"]);
+    }
+
+    _createPipelineInfrastructure(pipelineName: string, processors: string[], createSubfolder: boolean = true) {
+        let pipelineDestination = createSubfolder ? `./${pipelineName}/` : './';
+        let processorDestination = pipelineDestination + 'processors/';
+        
+        this._createAbstractProcessor(pipelineDestination, pipelineName);
+        this._createPipeline(pipelineName, processors, pipelineDestination);
+        this._createArguments();
+
+        for (const processorName of processors) {
+            this._createProcessor(processorDestination, processorName, pipelineName);
         }
+    }
+
+    _createArguments() {
+
+    }
+
+    _createPipeline(name: string, processors: string[], destination: string) {
+        let fileName = name.endsWith('.ts') ? name : name + '.ts';
+        fileName =
+            fileName.endsWith('Pipeline.ts')
+                ? fileName
+                : fileName.substring(0, fileName.length - 3) + 'Pipeline.ts';
 
         this.fs.copyTpl(
-            this.templatePath('_pipeline.ts.txt'),
-            this.destinationPath(pipelineName + '.ts'),
+            this.templatePath('_pipeline.ts.ejs'),
+            this.destinationPath(destination + fileName),
+            {
+                'pipelineName': name,
+                'processors': processors
+            },
+            {});
+    }
+
+    _createAbstractProcessor(destination: string, pipelineName: string) {
+        let name = pipelineName + 'Processor';
+
+        this.fs.copyTpl(
+            this.templatePath('_abstractProcessor.ts.ejs'),
+            this.destinationPath(destination + name + '.ts'),
+            {
+                'pipelineName': pipelineName
+            },
+            {});
+    }
+
+    _createProcessor(destination: string, name: string, pipelineName: string) {
+
+        this.fs.copyTpl(
+            this.templatePath('_predefinedProcessor.ts.ejs'),
+            this.destinationPath(destination + name + '.ts'),
             {
                 'pipelineName': pipelineName,
-                'processors': processorNameStrings
+                'processorName': name
             },
             {});
     }
