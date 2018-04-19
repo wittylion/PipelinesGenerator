d45376ffe2c9f5aa22f4098e64220a40e4bdc51e
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-04-19 14:30:05 +0300

    Introduced a self-complete csharp pipeline generator.

---
+++ csp/index.ts
@@ -0,0 +1,3 @@
+import { CSharpYeomanPipeline } from "../src/project/cs/CSharpYeomanPipeline";
+
+export = CSharpYeomanPipeline
--- src/project/app/ChooseProject/processors/ComposeListOfAvailableProjects.ts
+++ src/project/app/ChooseProject/processors/ComposeListOfAvailableProjects.ts
@@ -8,6 +8,7 @@ export class ComposeListOfAvailableProjects extends ChooseProjectProcessor {
     public async SafeExecute(args: ChooseProjectArguments): Promise<void> {
         args.AvailableProjects.push(new ProjectModel("ts", "Typescript language"));
         args.AvailableProjects.push(new ProjectModel("cs", "C# language"));
+        args.AvailableProjects.push(new ProjectModel("csp", "C# language [Pipelines.Net boosted]"));
         args.AvailableProjects.push(new ProjectModel("sc", "Sitecore framework"));
     }
 
